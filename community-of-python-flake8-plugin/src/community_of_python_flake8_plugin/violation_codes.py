588939bd9998d335330bab14bfb46d4dc60b2b54
Lev Vereshchagin <mail@vrslev.com>
2026-02-03 15:17:55 +0300

    Update

--- community-of-python-flake8-plugin/src/community_of_python_flake8_plugin/violation_codes.py
+++ community-of-python-flake8-plugin/src/community_of_python_flake8_plugin/violation_codes.py
@@ -1,65 +1,52 @@
 from __future__ import annotations
-
+import typing
+from enum import Enum
 from typing import TypedDict
 
 
-class ViolationCode(TypedDict):
+class ViolationCodeItem(TypedDict):
     code: str
     description: str
 
 
-VIOLATION_CODES: list[ViolationCode] = [
-    {
+class ViolationCode(Enum):
+    MODULE_IMPORT: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP001",
         "description": "Use module import when importing more than two names",
-    },
-    {
+    }
+    STDLIB_IMPORT: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP002",
         "description": "Import standard library modules as whole modules",
-    },
-    {
+    }
+    SCALAR_ANNOTATION: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP003",
         "description": "Avoid explicit scalar type annotations",
-    },
-    {
+    }
+    NAME_LENGTH: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP004",
         "description": "Name must be at least 8 characters",
-    },
-    {
+    }
+    FUNCTION_VERB: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP005",
         "description": "Function name must be a verb",
-    },
-    {
+    }
+    ASYNC_GET_PREFIX: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP006",
         "description": "Avoid get_ prefix in async function names",
-    },
-    {
+    }
+    TEMPORARY_VARIABLE: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP007",
         "description": "Avoid temporary variables used only once",
-    },
-    {
+    }
+    FINAL_CLASS: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP008",
         "description": "Classes should be marked typing.final",
-    },
-    {
+    }
+    MAPPING_PROXY: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP009",
         "description": "Wrap module dictionaries with types.MappingProxyType",
-    },
-    {
+    }
+    DATACLASS_CONFIG: typing.ClassVar[ViolationCodeItem] = {
         "code": "COP010",
         "description": "Use dataclasses with kw_only=True, slots=True, frozen=True",
-    },
-]
-
-
-def get_violation_description(code: str) -> str | None:
-    """Get the description for a given violation code."""
-    for violation in VIOLATION_CODES:
-        if violation["code"] == code:
-            return violation["description"]
-    return None
-
-
-def get_all_violation_codes() -> list[str]:
-    """Get all violation codes."""
-    return [violation["code"] for violation in VIOLATION_CODES]
+    }
